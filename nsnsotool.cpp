4013341ae3a80320361a74f0da6df188f0431cde
0CBH0 <maodatou88@163.com>
2018-07-12 21:16:03 +0800

    1.0a

---
+++ nsnsotool.cpp
@@ -0,0 +1,397 @@
+#include <io.h>
+#include <string.h>
+#include <vector>
+#include <algorithm> 
+#include <lz4.h>
+#include "nsnsotool.h"
+
+#pragma warning(disable : 4996)
+
+using namespace std;
+
+int decompress(FILE *in, FILE *out)
+{
+	NSOHeader nsoHeader;
+	fread(&nsoHeader, sizeof(NSOHeader), 1, in);
+	nsoHeader.flags = 0;
+	fwrite(&nsoHeader, sizeof(NSOHeader), 1, out);
+	tempa_u32 = 0;
+	for (u32 i = 0; i < 0x18; i++) fwrite(&tempa_u32, 4, 1, out);
+	while ((u32)ftell(out) < nsoHeader.fileOffset_text) putc(0, out);
+	char *textData = new char[nsoHeader.cmpSize_text];
+	char *dcmpTextData = new char[nsoHeader.dcmpSize_text];
+	fseek(in, nsoHeader.fileOffset_text, 0);
+	fread(textData, 1, nsoHeader.cmpSize_text, in);
+	LZ4_decompress_safe(textData, dcmpTextData, nsoHeader.cmpSize_text, nsoHeader.dcmpSize_text);
+	fwrite(dcmpTextData, 1, nsoHeader.dcmpSize_text, out);
+	nsoHeader.cmpSize_text = nsoHeader.dcmpSize_text;
+	char *rodataData = new char[nsoHeader.cmpSize_rodata];
+	char *dcmpRodataData = new char[nsoHeader.dcmpSize_rodata];
+	fseek(in, nsoHeader.fileOffset_rodata, 0);
+	nsoHeader.fileOffset_rodata = ftell(out);
+	fread(rodataData, 1, nsoHeader.cmpSize_rodata, in);
+	LZ4_decompress_safe(rodataData, dcmpRodataData, nsoHeader.cmpSize_rodata, nsoHeader.dcmpSize_rodata);
+	fwrite(dcmpRodataData, 1, nsoHeader.dcmpSize_rodata, out);
+	nsoHeader.cmpSize_rodata = nsoHeader.dcmpSize_rodata;
+	char *dataData = new char[nsoHeader.cmpSize_data];
+	char *dcmpDataData = new char[nsoHeader.dcmpSize_data];
+	fseek(in, nsoHeader.fileOffset_data, 0);
+	nsoHeader.fileOffset_data = ftell(out);
+	fread(dataData, 1, nsoHeader.cmpSize_data, in);
+	LZ4_decompress_safe(dataData, dcmpDataData, nsoHeader.cmpSize_data, nsoHeader.dcmpSize_data);
+	fwrite(dcmpDataData, 1, nsoHeader.dcmpSize_data, out);
+	nsoHeader.cmpSize_data = nsoHeader.dcmpSize_data;
+	rewind(out);
+	fwrite(&nsoHeader, sizeof(NSOHeader), 1, out);
+	delete[]textData;
+	delete[]dcmpTextData;
+	delete[]rodataData;
+	delete[]dcmpRodataData;
+	delete[]dataData;
+	delete[]dcmpDataData;
+	return 0;
+}
+
+int compress(FILE *in, FILE *out)
+{
+	u32 sha256_text[8];
+	u32 sha256_rodata[8];
+	u32 sha256_data[8];
+	NSOHeader nsoHeader;
+	fread(&nsoHeader, sizeof(NSOHeader), 1, in);
+	nsoHeader.flags = 0x3F;
+	fwrite(&nsoHeader, sizeof(NSOHeader), 1, out);
+	tempa_u32 = 0;
+	for (u32 i = 0; i < 0x18; i++) fwrite(&tempa_u32, 4, 1, out);
+	while ((u32)ftell(out) < nsoHeader.fileOffset_text) putc(0, out);
+	char *dcmpTextData = new char[nsoHeader.dcmpSize_text];
+	fseek(in, nsoHeader.fileOffset_text, 0);
+	fread(dcmpTextData, 1, nsoHeader.dcmpSize_text, in);
+	sha256_nso(dcmpTextData, nsoHeader.dcmpSize_text, sha256_text);
+	char *textData = new char[LZ4_compressBound(nsoHeader.dcmpSize_text)];
+	nsoHeader.cmpSize_text = LZ4_compress_default(dcmpTextData, textData, nsoHeader.dcmpSize_text, LZ4_compressBound(nsoHeader.dcmpSize_text));
+	fwrite(textData, 1, nsoHeader.cmpSize_text, out);
+	char *dcmpRodataData = new char[nsoHeader.dcmpSize_rodata];
+	fseek(in, nsoHeader.fileOffset_rodata, 0);
+	nsoHeader.fileOffset_rodata = ftell(out);
+	fread(dcmpRodataData, 1, nsoHeader.dcmpSize_rodata, in);
+	sha256_nso(dcmpRodataData, nsoHeader.dcmpSize_rodata, sha256_rodata);
+	char *rodataData = new char[LZ4_compressBound(nsoHeader.dcmpSize_rodata)];
+	nsoHeader.cmpSize_rodata = LZ4_compress_default(dcmpRodataData, rodataData, nsoHeader.dcmpSize_rodata, LZ4_compressBound(nsoHeader.dcmpSize_rodata));
+	fwrite(rodataData, 1, nsoHeader.cmpSize_rodata, out);
+	char *dcmpDataData = new char[nsoHeader.dcmpSize_data];
+	fseek(in, nsoHeader.fileOffset_data, 0);
+	nsoHeader.fileOffset_data = ftell(out);
+	fread(dcmpDataData, 1, nsoHeader.dcmpSize_data, in);
+	sha256_nso(dcmpDataData, nsoHeader.dcmpSize_data, sha256_data);
+	char *dataData = new char[LZ4_compressBound(nsoHeader.dcmpSize_data)];
+	nsoHeader.cmpSize_data = LZ4_compress_default(dcmpDataData, dataData, nsoHeader.dcmpSize_data, LZ4_compressBound(nsoHeader.dcmpSize_data));
+	fwrite(dataData, 1, nsoHeader.cmpSize_data, out);
+	rewind(out);
+	fwrite(&nsoHeader, sizeof(NSOHeader), 1, out);
+	fwrite(&sha256_text, 1, 0x20, out);
+	fwrite(&sha256_rodata, 1, 0x20, out);
+	fwrite(&sha256_data, 1, 0x20, out);
+	delete[]textData;
+	delete[]dcmpTextData;
+	delete[]rodataData;
+	delete[]dcmpRodataData;
+	delete[]dataData;
+	delete[]dcmpDataData;
+	return 0;
+
+
+
+
+	/*Elf32_Ehdr ehdr;
+	for(u32 i = 0; i < 0x10; i++)
+		ehdr.e_ident[i] = getc(in);
+	tempa_u32 = ehdr.e_ident[0]<<24|ehdr.e_ident[1]<<16|ehdr.e_ident[2]<<8|ehdr.e_ident[3];
+	if(tempa_u32!=0x7F454C46) return -1;
+	fread16_BE(ehdr.e_type, in);
+	if(ehdr.e_type != 0xFE01) return -1;
+	fread16_BE(ehdr.e_machine, in);
+	fread32_BE(ehdr.e_version, in);
+	fread32_BE(ehdr.e_entry, in);
+	fread32_BE(ehdr.e_phoff, in);
+	fread32_BE(ehdr.e_shoff, in);
+	fread32_BE(ehdr.e_flags, in);
+	fread16_BE(ehdr.e_ehsize, in);
+	fread16_BE(ehdr.e_phentsize, in);
+	fread16_BE(ehdr.e_phnum, in);
+	fread16_BE(ehdr.e_shentsize, in);
+	fread16_BE(ehdr.e_shnum, in);
+	fread16_BE(ehdr.e_shstrndx, in);
+	ulg shdr_data_elf_offset = ehdr.e_shoff + ehdr.e_shnum * ehdr.e_shentsize;
+	fwrite(ehdr.e_ident, sizeof(ehdr.e_ident), 1, out);
+	fwrite16_BE(ehdr.e_type, out);
+	fwrite16_BE(ehdr.e_machine, out);
+	fwrite32_BE(ehdr.e_version, out);
+	fwrite32_BE(ehdr.e_entry, out);
+	fwrite32_BE(ehdr.e_phoff, out);
+	fwrite32_BE(ehdr.e_shoff, out);
+	fwrite32_BE(ehdr.e_flags, out);
+	fwrite16_BE(ehdr.e_ehsize, out);
+	fwrite16_BE(ehdr.e_phentsize, out);
+	fwrite16_BE(ehdr.e_phnum, out);
+	fwrite16_BE(ehdr.e_shentsize, out);
+	fwrite16_BE(ehdr.e_shnum, out);
+	fwrite16_BE(ehdr.e_shstrndx, out);
+	fwrite32_BE(0x00000000, out);
+	fwrite32_BE(0x00000000, out);
+	fwrite32_BE(0x00000000, out);
+	ulg crc_data_offset = 0; 
+	u32 *crcs = new u32[ehdr.e_shnum];
+	vector< Elf32_Shdr_Sort > shdr_table_index;
+	Elf32_Shdr *shdr_table = new Elf32_Shdr[ehdr.e_shnum];
+	while(ftell(out)<shdr_data_elf_offset) putc(0, out);
+	fseek(in, ehdr.e_shoff, 0);
+	for (u32 i=0; i<ehdr.e_shnum; i++)
+	{
+		crcs[i] = 0;
+		fread32_BE(shdr_table[i].sh_name, in);
+		fread32_BE(shdr_table[i].sh_type, in);
+		fread32_BE(shdr_table[i].sh_flags, in);
+		fread32_BE(shdr_table[i].sh_addr, in);
+		fread32_BE(shdr_table[i].sh_offset, in);
+		fread32_BE(shdr_table[i].sh_size, in);
+		fread32_BE(shdr_table[i].sh_link, in);
+		fread32_BE(shdr_table[i].sh_info, in);
+		fread32_BE(shdr_table[i].sh_addralign, in);
+		fread32_BE(shdr_table[i].sh_entsize, in);
+		if (shdr_table[i].sh_offset != 0)
+		{
+			Elf32_Shdr_Sort shdr_index;
+			shdr_index.index = i;
+			shdr_index.sh_offset = shdr_table[i].sh_offset;
+			shdr_table_index.push_back(shdr_index);
+		}
+	}
+	sort(shdr_table_index.begin(), shdr_table_index.end(), SortFunc);
+	for(vector<Elf32_Shdr_Sort>::iterator shdr_index=shdr_table_index.begin();shdr_index!=shdr_table_index.end();shdr_index++)
+	{
+		pos = shdr_table[shdr_index->index].sh_offset; fseek(in, pos, 0);
+		shdr_table[shdr_index->index].sh_offset = ftell(out);
+		if (((shdr_table[shdr_index->index].sh_type & SHT_RPL_FILEINFO) == SHT_RPL_FILEINFO)||
+			((shdr_table[shdr_index->index].sh_type & SHT_RPL_CRCS) == SHT_RPL_CRCS)||
+			((shdr_table[shdr_index->index].sh_flags & SHF_RPL_ZLIB) == SHF_RPL_ZLIB))
+		{
+			u32 data_size = shdr_table[shdr_index->index].sh_size;
+			u32 block_size = CHUNK;
+			while(data_size>0)
+			{
+				u8 data[CHUNK];
+				block_size = CHUNK;
+				if(data_size<block_size)
+					block_size = data_size;
+				data_size -= block_size;
+				fread(data, 1, block_size, in);
+				fwrite(data, 1, block_size, out);
+				crcs[shdr_index->index] = crc32_rpx(crcs[shdr_index->index], data, block_size);
+			}
+		}
+		else
+		{
+			u32 data_size = shdr_table[shdr_index->index].sh_size;
+			u32 block_size = CHUNK;
+			u32 have;
+			z_stream strm;
+			u8 buff_in[CHUNK];
+			u8 buff_out[CHUNK];
+			strm.zalloc = Z_NULL;
+			strm.zfree = Z_NULL;
+			strm.opaque = Z_NULL;
+			strm.avail_in = 0;
+			strm.next_in = Z_NULL;
+			if(data_size<CHUNK)
+			{
+				block_size = data_size;
+				deflateInit(&strm, LEVEL);
+				strm.avail_in = fread(buff_in, 1, block_size, in);
+				crcs[shdr_index->index] = crc32_rpx(crcs[shdr_index->index], buff_in, block_size);
+				strm.next_in = buff_in;
+				strm.avail_out = CHUNK;
+				strm.next_out = buff_out;
+				deflate(&strm, Z_FINISH);
+				have = CHUNK - strm.avail_out;
+				if(have+4 < block_size)
+				{
+					fwrite32_BE(data_size, out);
+					fwrite(buff_out, 1, have, out);
+					shdr_table[shdr_index->index].sh_size = have+4;
+					shdr_table[shdr_index->index].sh_flags |= SHF_RPL_ZLIB;
+				}
+				else
+					fwrite(buff_in, 1, block_size, out);
+				deflateEnd(&strm);
+			}
+			else
+			{
+				s32 flush = Z_NO_FLUSH;
+				u32 compress_size = 4;
+				fwrite32_BE(data_size, out);
+				deflateInit(&strm, LEVEL);
+				while(data_size>0)
+				{
+					block_size = CHUNK;
+					flush = Z_NO_FLUSH;
+					if(data_size <= block_size)
+					{
+						block_size = data_size;
+						flush = Z_FINISH;
+					}
+					data_size -= block_size;
+					strm.avail_in = fread(buff_in, 1, block_size, in);
+					crcs[shdr_index->index] = crc32_rpx(crcs[shdr_index->index], buff_in, block_size);
+					strm.next_in = buff_in;
+					do{
+						strm.avail_out = CHUNK;
+						strm.next_out = buff_out;
+						deflate(&strm, flush);
+						have = CHUNK - strm.avail_out;
+						fwrite(buff_out, 1, have, out);
+						compress_size += have;
+					}while(strm.avail_out == 0);
+				}
+				deflateEnd(&strm);
+				shdr_table[shdr_index->index].sh_size = compress_size;
+				shdr_table[shdr_index->index].sh_flags |= SHF_RPL_ZLIB;
+			}
+		}
+		while(ftell(out)%0x40!=0) putc(0, out);
+		if((shdr_table[shdr_index->index].sh_type & SHT_RPL_CRCS) == SHT_RPL_CRCS)
+		{
+			crcs[shdr_index->index] = 0;
+			crc_data_offset = shdr_table[shdr_index->index].sh_offset;
+		}
+	}
+	fseek(out, ehdr.e_shoff, 0);
+	for (u32 i=0; i<ehdr.e_shnum; i++)
+	{
+		fwrite32_BE(shdr_table[i].sh_name, out);
+		fwrite32_BE(shdr_table[i].sh_type, out);
+		fwrite32_BE(shdr_table[i].sh_flags, out);
+		fwrite32_BE(shdr_table[i].sh_addr, out);
+		fwrite32_BE(shdr_table[i].sh_offset, out);
+		fwrite32_BE(shdr_table[i].sh_size, out);
+		fwrite32_BE(shdr_table[i].sh_link, out);
+		fwrite32_BE(shdr_table[i].sh_info, out);
+		fwrite32_BE(shdr_table[i].sh_addralign, out);
+		fwrite32_BE(shdr_table[i].sh_entsize, out);
+	}
+
+	fseek(out, crc_data_offset, 0);
+	for (u32 i=0; i<ehdr.e_shnum; i++)
+		fwrite32_BE(crcs[i], out);
+	delete[]crcs;
+	delete[]shdr_table;
+	shdr_table_index.clear();*/
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc < 2)
+	{
+		printf("nsnsotool\n");
+		printf("Compress or decompress NSO/NRO files for Nintendo Switch\n\n");
+		printf("Usage:\n");
+		printf("nsnsotool <file_name> [out_name]\n");
+		return 0;
+	}
+	FILE *in = fopen(argv[1], "rb");
+	if(in == NULL) return -1;
+	fseek(in, 0xC, 0);
+	u32 flags = 0;
+	fread(&flags, 4, 1, in);
+	rewind(in);
+	FILE *out;
+	if(argc == 2)
+		out = fopen("temp.bin", "wb");
+	else
+	{
+		out = fopen(argv[2], "wb");
+		if (out == NULL) return -1;
+	}
+	s32 result = -1;
+	switch (flags)
+	{
+	case 0:
+		printf("compressing...\n");
+		result = compress(in, out);
+		break;
+	case 0x3F:
+		printf("decompressing...\n");
+		result = decompress(in, out);
+		break;
+	default:
+		printf("unsupported flags 0x%08X!\n", flags);
+	}
+	fclose(in);
+	fclose(out);
+	if(argc == 2 && result == 0)
+		fcopy((char *)"temp.bin", argv[1]);
+	remove("temp.bin");
+	return 0;
+}
+
+void sha256_nso(char *data, long length, u32 *sha256)
+{
+	char *pp, *ppend;
+	long l, i, W[64], T1, T2, A, B, C, D, E, F, G, H;
+	sha256[0] = 0x6a09e667, sha256[1] = 0xbb67ae85, sha256[2] = 0x3c6ef372, sha256[3] = 0xa54ff53a;
+	sha256[4] = 0x510e527f, sha256[5] = 0x9b05688c, sha256[6] = 0x1f83d9ab, sha256[7] = 0x5be0cd19;
+	u32 K[64] = {
+		0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+		0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+		0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+		0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+		0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+		0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+		0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+		0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+	};
+	l = length + ((length % 64 > 56) ? (128 - length % 64) : (64 - length % 64));
+	pp = (char*)malloc((unsigned long)l);
+	for (i = 0; i < length; pp[i + 3 - 2 * (i % 4)] = data[i], i++);
+	for (pp[i + 3 - 2 * (i % 4)] = (char)128, i++; i < l; pp[i + 3 - 2 * (i % 4)] = 0, i++);
+	*((long*)(pp + l - 4)) = length << 3;
+	*((long*)(pp + l - 8)) = length >> 29;
+	for (ppend = pp + l; pp < ppend; pp += 64) {
+		for (i = 0; i < 16; W[i] = ((long*)pp)[i], i++);
+		for (i = 16; i < 64; W[i] = (SHA256_O1(W[i - 2]) + W[i - 7] + SHA256_O0(W[i - 15]) + W[i - 16]), i++);
+		A = sha256[0], B = sha256[1], C = sha256[2], D = sha256[3], E = sha256[4], F = sha256[5], G = sha256[6], H = sha256[7];
+		for (i = 0; i < 64; i++) {
+			T1 = H + SHA256_E1(E) + SHA256_Ch(E, F, G) + K[i] + W[i];
+			T2 = SHA256_E0(A) + SHA256_Maj(A, B, C);
+			H = G, G = F, F = E, E = D + T1, D = C, C = B, B = A, A = T1 + T2;
+		}
+		sha256[0] += A, sha256[1] += B, sha256[2] += C, sha256[3] += D, sha256[4] += E, sha256[5] += F, sha256[6] += G, sha256[7] += H;
+	}
+	free(pp - l);
+	for (i = 0; i < 8; i++) sha256[i] = (sha256[i] & 0xFF000000) >> 24 | (sha256[i] & 0xFF0000) >> 8 | (sha256[i] & 0xFF00) << 8 | (sha256[i] & 0xFF) << 24;
+}
+
+int fcopy(char *src_name, char *dest_name)
+{
+	FILE *src=fopen(src_name, "rb");
+	if(src == NULL) return -1;
+	FILE *dest=fopen(dest_name, "wb");
+	fseek(src, 0, 2);
+	unsigned int data_size=ftell(src);
+	rewind(src);
+	unsigned int block_size = 512;
+	while(data_size>0)
+	{
+		char data[512];
+		block_size = 512;
+		if(data_size<block_size)
+			block_size = data_size;
+		data_size -= block_size;
+		fread(data, 1, block_size, src);
+		fwrite(data, 1, block_size, dest);
+	}
+	fclose(src);
+	fclose(dest);
+	return 0;
+}
---
+++ nsnsotool.h
@@ -0,0 +1,87 @@
+// Copyright (C) 2018  0CBH0 <maodatou88@163.com>
+// Licensed under the terms of the GNU GPL, version 3
+// http://www.gnu.org/licenses/gpl-3.0.txt
+
+#pragma once
+
+#define SHF_RPL_ZLIB 		0x08000000
+
+#define SHT_PROGBITS        0x00000001
+#define SHT_SYMTAB          0x00000002
+#define SHT_STRTAB          0x00000003
+#define SHT_RELA            0x00000004
+#define SHT_NOBITS          0x00000008
+
+#define SHT_RPL_EXPORTS     0x80000001
+#define SHT_RPL_IMPORTS     0x80000002
+#define SHT_RPL_CRCS        0x80000003
+#define SHT_RPL_FILEINFO 	0x80000004
+
+#define CHUNK 16384
+#define LEVEL 6
+
+#define SHA256_ROTL(a,b) (((a>>(32-b))&(0x7fffffff>>(31-b)))|(a<<b))
+#define SHA256_SR(a,b) ((a>>b)&(0x7fffffff>>(b-1)))
+#define SHA256_Ch(x,y,z) ((x&y)^((~x)&z))
+#define SHA256_Maj(x,y,z) ((x&y)^(x&z)^(y&z))
+#define SHA256_E0(x) (SHA256_ROTL(x,30)^SHA256_ROTL(x,19)^SHA256_ROTL(x,10))
+#define SHA256_E1(x) (SHA256_ROTL(x,26)^SHA256_ROTL(x,21)^SHA256_ROTL(x,7))
+#define SHA256_O0(x) (SHA256_ROTL(x,25)^SHA256_ROTL(x,14)^SHA256_SR(x,3))
+#define SHA256_O1(x) (SHA256_ROTL(x,15)^SHA256_ROTL(x,13)^SHA256_SR(x,10))
+
+typedef unsigned char	u8;
+typedef unsigned short	u16;
+typedef unsigned int	u32;
+typedef signed   char	s8;
+typedef signed   short	s16;
+typedef signed   int    s32;
+typedef unsigned long	ulg;
+typedef unsigned long long u64;
+typedef signed long long   s64;
+
+u8  tempa_u8,
+    tempb_u8;
+
+u16 tempa_u16,
+    tempb_u16;
+
+u32 tempa_u32,
+    tempb_u32;
+
+ulg pos;
+
+typedef struct {
+	u32 magic;
+	u32 version;
+	u32 pada;
+	u32 flags;
+	u32 fileOffset_text;
+	u32 memoryOffset_text;
+	u32 dcmpSize_text;
+	u32 modOffset;
+	u32 fileOffset_rodata;
+	u32 memoryOffset_rodata;
+	u32 dcmpSize_rodata;
+	u32 modSize;
+	u32 fileOffset_data;
+	u32 memoryOffset_data;
+	u32 dcmpSize_data;
+	u32 bssSize;
+	char note[0x20];
+	u32 cmpSize_text;
+	u32 cmpSize_rodata;
+	u32 cmpSize_data;
+	char padb[0x1C];
+	u32 rdOffset_api;
+	u32 rdSize_api;
+	u32 rdOffset_dynstr;
+	u32 rdSize_dynstr;
+	u32 rdOffset_dynsym;
+	u32 rdSize_dynsym;
+} NSOHeader;
+
+void sha256_nso(char *data, long length, u32 *sha256);
+int fcopy(char *src_name, char *dest_name);
+
+int decompress(FILE *in, FILE *out);
+int compress(FILE *in, FILE *out);
